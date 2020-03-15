2cbf349f8132942ef2dedb154d5bd7f684385e9b
alejandro.bullrich <alejandro.bullrich@playtika.com>
2020-03-15 18:22:40 -0300

    create-project-initial-structure
    * Initial commit.
    * Adds application endpoint.
    * Adds StoreService.

---
+++ src/main/java/com/abullrich/crud/model/Sucursal.java
@@ -0,0 +1,12 @@
+package com.abullrich.crud.model;
+
+import lombok.Value;
+
+@Value
+public class Sucursal {
+
+    String id;
+    String address;
+    Double latitude;
+    Double longitude;
+}
---
+++ src/main/java/com/abullrich/crud/service/StoreService.java
@@ -0,0 +1,18 @@
+package com.abullrich.crud.service;
+
+import com.abullrich.crud.model.Sucursal;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StoreService {
+
+    public Sucursal getStoreById(final String storeId) {
+        return null;
+    }
+
+    public void createStore(final Sucursal sucursal) {
+    }
+
+    public void updateStore(final Sucursal sucursal) {
+    }
+}
